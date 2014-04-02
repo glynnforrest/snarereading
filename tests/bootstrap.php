e80bf5b9e9c4f011acb8e87b3b48b061a3c6cc88
Glynn Forrest <me@glynnforrest.com>
2014-04-02 23:43:38 +0000

    Adding phpunit.xml.dist and bootstrap.

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!$loader = @include __DIR__ . '/../vendor/autoload.php') {
+    echo 'Composer autoloader not found.' . PHP_EOL;
+    echo 'Please run `composer install`.' . PHP_EOL;
+    exit(1);
+}
+
+$loader->add('SnareReading\Tests', __DIR__);
